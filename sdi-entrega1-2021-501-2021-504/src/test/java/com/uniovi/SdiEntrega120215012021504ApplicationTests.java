c72b7bc0e493df416919cc61c69a04d754ed0fac
Eric Almeda Tomás <UO269947@uniovi.es>
2021-03-08 13:28:38 +0100

    Primer commit

---
+++ sdi-entrega1-2021-501-2021-504/src/test/java/com/uniovi/SdiEntrega120215012021504ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiEntrega120215012021504ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
